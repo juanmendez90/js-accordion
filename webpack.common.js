65ee800e001361d89b8a8cfa4ddac06e8fc4b232
Juan <juan.mendez@guidance.com>
2018-11-30 16:13:45 +0100

    stlyes refactor

--- webpack.common.js
+++ webpack.common.js
@@ -1,79 +1,94 @@
-const path = require('path');
-const autoprefixer = require('autoprefixer');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const publicFilesPath = path.resolve(__dirname, 'public');
+const publicFilesPath = path.resolve(__dirname, "public");
 
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
 module.exports = {
-   entry: {
-      app: './src/index.js'
-   },
-   plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
-         template: path.resolve(publicFilesPath, 'index.html')
-      })
-   ],
-   output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist')
-   },
-   module: {
+  entry: {
+    app: "./src/index.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(publicFilesPath, "index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].bundle.css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, '../src'),
+        include: path.resolve(__dirname, "../src"),
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader"
         }
       },
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       },
       {
         test: sassRegex,
         exclude: sassModuleRegex,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 2
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: "sass-loader"
           }
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            plugins: () => [autoprefixer()]
-          }
-        }, {
-          loader: 'sass-loader'
-        }
-      ]},
+        ]
+      },
       {
         test: sassModuleRegex,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 2,
-            modules: true,
-            localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              modules: true,
+              localIdentName: "[path]___[name]__[local]___[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: "sass-loader"
           }
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            plugins: () => [autoprefixer()]
-          }
-        }, {
-          loader: 'sass-loader'
-        }]
+        ]
       }
-    ],
+    ]
   }
 };
