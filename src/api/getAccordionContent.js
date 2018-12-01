790da4579ba19d2abe90c9d54f17b5b15db7d4b9
Juan <juan.mendez@guidance.com>
2018-12-01 12:01:45 +0100

    fixed strings

--- src/api/getAccordionContent.js
+++ src/api/getAccordionContent.js
@@ -3,5 +3,5 @@ import { get } from "./api";
 export const getAccordionContent = () => {
   return get("/posts/1/comments?id=1")
     .then(result => result && result.length && result[0].body)
-    .catch(error => console.log("Error: ", error));
+    .catch(error => console.log("Error getAccordionContent: ", error));
 };
